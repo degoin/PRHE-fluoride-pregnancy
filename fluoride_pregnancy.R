e697afde22a84d56b1ad247841f4ebab7620adb8
Dana Goin <danaegoin@gmail.com>
2019-06-27 13:33:44 -0700

    Add boxplots

--- fluoride_pregnancy.R
+++ fluoride_pregnancy.R
@@ -1,6 +1,8 @@
 # fluoride levels in pregnant women analyses 
 library(readxl)
 library(dplyr)
+library(ggplot2)
+library(tidyverse)
 
 # read in data 
 
@@ -9,4 +11,20 @@ df <- read_excel('/Users/danagoin/Documents/Fluoride and pregnant women/Data.xls
 df <- df %>% rename(smoker=`Smoker?`, water_fluoride=`Water Fluoride`, mat_urine = `maternal urine F MEAN (ppm)`, 
                     amniotic_fluid = `amniotic fluid MEAN (ppm)`, serum_fluoride = `Serum Fluoride (ppm)`)
 
-df$amniotic_fluid <- as.numeric(df$amniotic_fluid)
+df$amniotic_fluid <- as.numeric(df$amniotic_fluid) 
+
+df_l <- df %>% gather(key="measure",value="concentration", water_fluoride, 
+                      mat_urine, amniotic_fluid, serum_fluoride)
+
+
+ggplot(df_l, aes(x=measure, y=concentration)) + geom_boxplot()
+
+ggplot(df_l %>% filter(measure==c("mat_urine","water_fluoride")), aes(x=measure, y=concentration)) + 
+    theme_bw()  + geom_boxplot() + labs(x="",y="Mean concentration (ppm)") + 
+    scale_x_discrete(labels=c("Maternal urine","Water"))
+
+ggplot(df_l %>% filter(measure==c("serum_fluoride","amniotic_fluid")), aes(x=measure, y=concentration)) + 
+  theme_bw()  + geom_boxplot() + labs(x="",y="Mean concentration (ppm)") + 
+  scale_x_discrete(labels=c("Serum fluoride","Amniotic fluid"))
+
+
