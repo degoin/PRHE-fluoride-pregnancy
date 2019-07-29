39a51d15e6f723a9cdf08805eb22e5abba6352dc
Dana Goin <danaegoin@gmail.com>
2019-07-29 09:29:55 -0700

    Successful test

--- scripts/fluoride_pregnancy.R
+++ scripts/fluoride_pregnancy.R
@@ -5,7 +5,7 @@ library(dplyr)
 library(ggplot2)
 library(tidyverse)
 
-# read in data -- testing
+# read in data 
 
 df <- read_excel('/Users/danagoin/Box Sync/Fluoride and pregnant women/data/Data.xlsx')
 
